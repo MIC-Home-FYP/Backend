3a5048102958b4cd75550a3288ac14ba4de2ba46
Green-Tea-123 <junboonsoo@gmail.com>
2025-03-10 22:59:01 +0800

    Set up firebase backend (not currently connected to main control flow)

---
+++ FYP/backend/firebase/firebase.py
@@ -0,0 +1,33 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import messaging
+
+class FirebaseMessenger:
+    cred = credentials.Certificate("FYP/backend/firebase/service-account.json")
+    firebaseApp = firebase_admin.initialize_app(cred)
+    
+
+    def sendToToken(self):
+        REGISTRATION_TOKEN = 'enfFQu4GRRa_xaJGyYMFN1:APA91bEJMjCy1bF0Kfv3v4EhEUoA6G6kyNkPirKBZQOH-d1eMMztmQ0ScU6IzZuily8DNC8_-Zl0NG7yA5REggG4LwVks49-HrfWRAAsNWyC_8ALfwUdZiI' # TODO make secret
+
+        message = messaging.Message(
+            data = {
+                'score': '850',
+                'time': '2:45',
+            },
+            notification = messaging.Notification(
+            title = "Test title",
+            body = "Test body"
+            ),
+            token = REGISTRATION_TOKEN,
+        )
+
+        response = messaging.send(message)
+        print('Successfully sent message:', response)
+
+def __main__():
+    messenger = FirebaseMessenger()
+    messenger.sendToToken()
+
+if __name__ == "__main__":
+    __main__()
